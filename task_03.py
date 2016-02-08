4b1d4ee69acb20fd84a141332db4d5090e464603
Sylvia Guzikowski <xiran-aneki@live.com>
2016-02-08 08:56:38 +0000

    done with task 03

--- task_03.py
+++ task_03.py
@@ -1,7 +1,7 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""module docstring of some sort"""
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
